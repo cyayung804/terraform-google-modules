683f497dcbeee3723b3335e7381b1d878ef67b8a
cyayung804 <cyayung804@gmail.com>
2026-04-23 04:08:43 +0000

    Update script safe checks

--- update.sh
+++ update.sh
@@ -38,15 +38,15 @@ function update_terraform_modules()
 
       echo "${module_json}" | jq -r '.root.readme // empty' > "${module_dir}/README.md"
 
-      echo "${module_json}" | jq -e '.root.inputs[]?' > /dev/null && \
+      echo "${module_json}" | jq -e '.root.inputs | length > 0' > /dev/null 2>&1 && \
         echo "${module_json}" | jq '.root.inputs' > "${module_dir}/input.json"
 
-      echo "${module_json}" | jq -e '.root.outputs[]?' > /dev/null && \
+      echo "${module_json}" | jq -e '.root.outputs | length > 0' > /dev/null 2>&1 && \
         echo "${module_json}" | jq '.root.outputs' > "${module_dir}/output.json"
 
       echo "      -> Checking for submodules..."
 
-      if echo "${module_json}" | jq -e '.submodules[]?' > /dev/null; then
+      if echo "${module_json}" | jq -e '.submodules | length > 0' > /dev/null 2>&1; then
 
         echo "${module_json}" | jq -c '.submodules[]' | while read -r submodule_json; do
 
@@ -62,10 +62,10 @@ function update_terraform_modules()
 
           echo "${submodule_json}" | jq -r '.readme // empty' > "${submodule_dir}/README.md"
 
-          echo "${submodule_json}" | jq -e '.inputs[]?' > /dev/null && \
+          echo "${submodule_json}" | jq -e '.inputs | length > 0' > /dev/null 2>&1 && \
             echo "${submodule_json}" | jq '.inputs' > "${submodule_dir}/input.json"
 
-          echo "${submodule_json}" | jq -e '.outputs[]?' > /dev/null && \
+          echo "${submodule_json}" | jq -e '.outputs | length > 0' > /dev/null 2>&1 && \
             echo "${submodule_json}" | jq '.outputs' > "${submodule_dir}/output.json"
 
         done
